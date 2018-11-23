2862655ae887ae3b744893aea5bb11fc7bc958e6
ADMIN\Admin <tranthithanhvan10397@gmail.com>
2018-11-23 15:21:39 +0700

    update app1 handlebar

--- frontend/app1/app.js
+++ frontend/app1/app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -18,7 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- frontend/app1/routes/index.js
+++ frontend/app1/routes/index.js
@@ -4,13 +4,18 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render('index',{
-  	title:'Express'
+  res.render('app',{
+  	title:'Request'
   });
 });
 
-router.get('/abc', function(req, res, next) {
-  res.render('app1');
+router.get('/request', function(req, res, next) {
+  var name = req.body.name;
+  var phone = req.body.phone;
+  var address = req.body.address;
+  var note = req.body.note;
+
+  console.log(`name = ${name} phone = ${phone} address = ${address} note = ${note}`)
 });
 
 module.exports = router;
